b1021c3752f172789e4122079fbfb7b0be8100bb
Maks <maxartem0419@gmail.com>
2021-04-22 15:22:45 +0300

    updaye_day 2

--- js/index.js
+++ js/index.js
@@ -6,9 +6,8 @@ const CLIENT_ID = "118018668082-irallb30fmncoc0n9uih2lp0rlq5dsek.apps.googleuser
 const SECRET = "Xkc-1KluTwdslirpazaLx7ya";
 
 const createCard = (dataVideo) => {
-        console.log(dataVideo.statistics);
         const imgUrl = dataVideo.snippet.thumbnails.high.url;
-        const videoId = dataVideo.id.videoId;
+        const videoId = typeof dataVideo.id === "string" ? dataVideo.id : dataVideo.id.videoId;
         const titleVideo = dataVideo.snippet.title;
         const dateVideo = dataVideo.snippet.publishedAt;
         const chanelTitle = dataVideo.snippet.channelTitle;
@@ -42,3 +41,68 @@ const createList = (wrapper, listVideo = []) => {
 createList(gloAcademyList, gloAcademy);
 createList(trendingList, trending);
 createList(musicList, music);
+
+// Реализация через API
+
+const authButton = document.querySelector(".auth-btn");
+const userAvatar = document.querySelector(".user-avatar");
+
+const handleSucessAuth = data => {
+    console.log(data);
+    authButton.classList.add("hide");
+    userAvatar.classList.remove('hide');
+    userAvatar.src = data.getImageUrl();
+    userAvatar.alt = '';
+    getChanel();
+}
+
+const handleNoAuth = () => {
+    authButton.classList.remove("hide");
+    userAvatar.classList.add('hide');
+    userAvatar.src = '';
+    userAvatar.alt = '';
+}
+
+const handleAuth = () => {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+const handleSignOut = () => {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+const updateStatusAuth = data => {
+    data.isSignedIn.listen(() => {
+        updateStatusAuth(data);
+    })
+    if (data.isSignedIn.get()) {
+        const userData = data.currentUser.get().getBasicProfile();
+        handleSucessAuth(userData);
+    } else {
+        handleNoAuth();
+    }
+};
+
+function initClient() {
+    gapi.client.init({
+        'clientId': CLIENT_ID,
+        'scope': 'https://www.googleapis.com/auth/youtube.readonly',
+        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+    }).then(() => {
+        updateStatusAuth(gapi.auth2.getAuthInstance());
+        authButton.addEventListener('click', handleAuth);
+        userAvatar.addEventListener('click', handleSignOut);
+    });
+  }
+
+  gapi.load('client:auth2', initClient);
+
+  const getChanel = () => {
+    gapi.client.youtube.channels.list({
+        part: 'snippet, contentDetails, statistics',
+        // ID канала
+        id: 'UCVswRUcKC-M35RzgPRv8qUg',
+    }).then((response) => {
+        console.log(response);
+    })
+  }
